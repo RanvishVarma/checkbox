950bc35d874fdbab8ab4baf4c4a477f27013b9df
Allvy <allvy@Allvys-MacBook-Pro-2.local>
2019-02-18 15:22:27 +0530

    first commit

--- checkbox/ViewController.swift
+++ checkbox/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var isAllSelected: Bool = false
+    
+    @IBOutlet weak var buttonTap: UIButton!
+    
+    @IBOutlet weak var selectTCButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func buttonTap(_ sender: Any) {
+        
+        if isAllSelected == false
+        {
+            //errorLabel.isHidden = true
+           // showAlert(msg:"Please Accept Terms Of Service")
+            let alert = UIAlertController(title: "Alert", message: "Please Accept Terms & Conditions", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        
+        
+    }
+    
+    @IBAction func checkboxAction(_ sender: UIButton) {
+        
+        let image = isAllSelected ? "check-box-empty" : "checked-box-42X42"
+        isAllSelected = !isAllSelected
+        selectTCButton.setImage(UIImage(named: image), for: .normal)
+        
+      
+        
+        
+    }
+    
 }
 
